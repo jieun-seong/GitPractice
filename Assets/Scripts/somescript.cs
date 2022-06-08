9d14593358b94f53b8d12eef82c4b264d8001052
Jieun Seong <jieun@Jieuns-MacBook-Air.local>
2022-06-08 18:46:23 -0400

    branch jieun2, commit1

--- Assets/Scripts/somescript.cs
+++ Assets/Scripts/somescript.cs
@@ -7,7 +7,7 @@ public class somescript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        print("Hello from Jieun :)");
+        print("Hello from Jieun2 :)");
     }
 
     // Update is called once per frame
